b4b6f2abfb3960cd8dc29cce4b3f63e5490ebd5c
Subhradeep Pal <subhradeeppal30@gmail.com>
2025-04-29 11:03:17 +0530

    changed file

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String [] args){
         System.out.println("Hello World!"); // Prints Hello World! to the console
-        System.out.println(""); // Prints This is a test. to the console
+        System.out.println("how are you ?"); // Prints This is a test. to the console
         System.out.println("Goodbye!"); // Prints Goodbye! to the console
     }
 }
